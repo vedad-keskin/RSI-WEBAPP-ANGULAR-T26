7ba681d4453dc3b35e5d28452ab48e7850801fd8
Vedad Keskin <vedad.keskin98@gmail.com>
2026-09-03 07:13:59 +0200

    done

--- Workshops/2026-08-26/rs1-frontend-2025-26/src/app/modules/admin/catalogs/product-offers/product-offers.component.ts
+++ Workshops/2026-08-26/rs1-frontend-2025-26/src/app/modules/admin/catalogs/product-offers/product-offers.component.ts
@@ -1,6 +1,16 @@
-import { Component, inject } from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
 import { Router } from '@angular/router';
-import { ProductOfferStateType } from '../../../../api-services/product-offers/product-offers-api.models';
+import {
+  ListProductOffersQueryDto, ListProductOffersRequest,
+  ProductOfferStateType
+} from '../../../../api-services/product-offers/product-offers-api.models';
+import {BaseListPagedComponent} from '../../../../core/components/base-classes/base-list-paged-component';
+import {ListProductsQueryDto, ListProductsRequest} from '../../../../api-services/products/products-api.models';
+import {ProductsApiService} from '../../../../api-services/products/products-api.service';
+import {ToasterService} from '../../../../core/services/toaster.service';
+import {DialogHelperService} from '../../../shared/services/dialog-helper.service';
+import {ProductOffersApiService} from '../../../../api-services/product-offers/product-offers-api.service';
+import {OrdersApiService} from '../../../../api-services/orders/orders-api.service';
 
 @Component({
   selector: 'app-product-offers',
@@ -8,21 +18,100 @@ import { ProductOfferStateType } from '../../../../api-services/product-offers/p
   templateUrl: './product-offers.component.html',
   styleUrl: './product-offers.component.scss',
 })
-export class ProductOffersComponent {
+export class ProductOffersComponent
+  extends BaseListPagedComponent<ListProductOffersQueryDto, ListProductOffersRequest>
+  implements OnInit
+{
+
+
   private readonly router = inject(Router);
+  private api = inject(ProductOffersApiService);
+  private toaster = inject(ToasterService);
+  private dialogHelper = inject(DialogHelperService);
+  private productsApi = inject(ProductsApiService);
+
+
+
+
+
 
   readonly displayedColumns = ['code', 'product', 'price', 'discount', 'discounted', 'validUntil', 'state', 'actions'];
-  readonly products: Array<{ id: number; name: string }> = [];
+  // readonly products: Array<{ id: number; name: string }> = [];
+
+  products: any;
+
+
+
+
+  constructor() {
+    super();
+    this.request = new ListProductsRequest();
+    this.request.paging.pageSize = 5;
+  }
+
+  ngOnInit(): void {
+    this.initList();
+    this.loadProducts();
+  }
+
+  protected loadPagedData(): void {
+    this.startLoading();
+
+    this.api.list(this.request).subscribe({
+      next: (response) => {
+        this.handlePageResult(response);
+        this.stopLoading();
+      },
+      error: (err) => {
+        this.stopLoading('Failed to load offers');
+        console.error('Load offers error:', err);
+      }
+    });
+  }
+
+  private loadProducts() {
+
+    this.startLoading();
+
+    this.productsApi.list().subscribe({
+      next: (response) => {
+
+        this.products = response.items;
+
+
+        this.stopLoading();
+      },
+      error: (err) => {
+        this.stopLoading('Failed to load offers');
+        console.error('Load offers error:', err);
+      }
+    });
+
+  }
+
+
+
+
+
+
 
   // Ispitni zadatak: zamijeniti demo redove rezultatom API poziva.
-  readonly rows = [
-    { id: 1, code: 'OFF-001', product: 'Tastatura', price: 20, discount: 10, discounted: 18, validUntil: '2026-09-05', state: ProductOfferStateType.Aktivna, stateLabel: 'Aktivna', isEnabled: true },
-    { id: 2, code: 'OFF-002', product: 'Tastatura', price: 20, discount: 15, discounted: 17, validUntil: '2026-08-25', state: ProductOfferStateType.Istekla, stateLabel: 'Istekla', isEnabled: true },
-    { id: 3, code: 'OFF-003', product: 'Tastatura', price: 20, discount: 20, discounted: 16, validUntil: '2026-09-15', state: ProductOfferStateType.Iskljucena, stateLabel: 'Iskljucena', isEnabled: false },
-  ];
+  // readonly rows = [
+  //   { id: 1, code: 'OFF-001', product: 'Tastatura', price: 20, discount: 10, discounted: 18, validUntil: '2026-09-05', state: ProductOfferStateType.Aktivna, stateLabel: 'Aktivna', isEnabled: true },
+  //   { id: 2, code: 'OFF-002', product: 'Tastatura', price: 20, discount: 15, discounted: 17, validUntil: '2026-08-25', state: ProductOfferStateType.Istekla, stateLabel: 'Istekla', isEnabled: true },
+  //   { id: 3, code: 'OFF-003', product: 'Tastatura', price: 20, discount: 20, discounted: 16, validUntil: '2026-09-15', state: ProductOfferStateType.Iskljucena, stateLabel: 'Iskljucena', isEnabled: false },
+  // ];
 
   add(): void { this.router.navigate(['/admin/product-offers/add']); }
   edit(id: number): void { this.router.navigate(['/admin/product-offers/edit', id]); }
   delete(id: number): void { void id; /* TODO: student implementira potvrdu i brisanje. */ }
-  filtersChanged(): void { /* TODO: student implementira filtere i povratak na prvu stranicu. */ }
+  filtersChanged(): void { /* TODO: student implementira filtere i povratak na prvu stranicu. */
+
+
+    this.request.paging.page = 1;
+    this.loadPagedData();
+
+  }
+
+
 }
